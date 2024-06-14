d7b9af5a380fd90732905635348e9966f3e30c0e
[abhinabaTogglehead,abhinaba900 <[abhinaba@togglehead.in,abhinabajana900@gmail.com>
2024-06-14 18:40:31 +0530

    summary aded

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,21 +22,19 @@ function About() {
               </strong>{" "}
               <br />
               <br />
-              As a dedicated full stack developer, I'm driven by the art of
-              crafting captivating user experiences through innovative web
-              interfaces and robust backend solutions. Proficient in HTML, CSS,
-              JavaScript, and server-side technologies, I specialize in creating
-              responsive and visually appealing designs while ensuring efficient
-              server performance. With expertise in frameworks like React,
-              Vue.js, Node.js, and Express, I bring concepts to life with clean
-              and efficient code. My portfolio showcases a range of projects,
-              each highlighting my ability to blend creativity with
-              functionality across the entire stack. I prioritize user-centric
-              design, aiming for seamless interactions and engaging interfaces,
-              while maintaining secure and scalable backend systems. Eager to
-              collaborate on projects that challenge conventions and push the
-              boundaries of full stack development, I'm committed to delivering
-              impactful digital experiences.
+              As a dedicated full stack developer, I craft captivating user
+              experiences with innovative web interfaces and robust backend
+              solutions. Proficient in HTML, CSS, JavaScript, and server-side
+              technologies, I specialize in creating responsive and visually
+              appealing designs while ensuring efficient server performance.
+              With expertise in frameworks like React, Vue.js, Node.js, and
+              Express, I bring concepts to life with clean, efficient code. My
+              portfolio showcases projects that blend creativity with
+              functionality across the stack. I prioritize user-centric design,
+              aiming for seamless interactions and engaging interfaces while
+              maintaining secure, scalable backend systems. Eager to collaborate
+              on challenging projects, I'm committed to delivering impactful
+              digital experiences.
             </p>
           </div>
           <div>
